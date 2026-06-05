a31e2e63549345f5504587a289c755d0827b9c91
mamkd <ma.mercade@duocuc.cl>
2026-06-05 10:41:38 -0400

    Se añadió un programita.
    Se añadió un programita.

---
+++ index.py
@@ -0,0 +1,10 @@
+import random
+
+print("**********************")
+print("100 números aleatorios")
+print("**********************")
+
+print()
+
+for k in range(100):
+    print(random.randint(0, 1000), end=" ")
